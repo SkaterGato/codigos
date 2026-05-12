73a28e4b5e863975d27acbc3dfaccc14d9e4e066
SkaterGato <diegojoelgalvezmolima@gmail.com>
2026-05-12 08:45:33 -0500

    Add files via upload

---
+++ SecuenciasDeEscape.java
@@ -0,0 +1,9 @@
+class SecuenciasDeEscape {
+    public static void main(String[] args) {
+        System.out.println("Texto 1\nTexto 2");          // Salto de línea
+        System.out.println("Texto 3\tTexto 4");          // Tabulación
+        System.out.println("Texto 5\rTexto 6");          // Retorno de carro
+        System.out.println("\"Texto 7\" Texto 8");       // Comillas dentro del texto
+        System.out.println("Texto 9\\ Texto 10");        // Barra invertida
+    } // Fin del método main
+} // Fin de la clase SecuenciasDeEscape
